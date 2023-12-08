ae4f7a5809583693d254cf5e1dc346212604e2a3
Mustafa Al-Katat <alkatat@csu.fullerton.edu>
2023-12-08 11:39:59 -0800

    Cleaned up code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -59,5 +62,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.google.android.gms:play-services-auth:18.0.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 }
